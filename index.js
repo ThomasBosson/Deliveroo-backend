ab52b49d8728f554da5e6dab85f327cdda28ed65
Thomas Bosson <thomasbosson@MacBook-Pro-de-Thomas.local>
2021-02-08 15:06:59 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+const data = require("./data.json");
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.listen(process.env.PORT || 3200, () => {
+  console.log("Server started");
+});
